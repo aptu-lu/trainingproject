430ab009756cbf96d45122c3a36408c3a7729243
aptu.lu <ssc94@inbox.ru>
2020-07-05 13:20:38 +0300

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,66 @@
+INSERT INTO Organization (name, full_name, inn, kpp, address, phone, is_active) VALUES ('ООО "ИМПУЛЬС"', 'Общество с ограниченной ответственностью "Импульс"', '457733210561', '334956843', 'ул. Ленина 15', '84952364343', true);
+
+INSERT INTO Organization (name, full_name, inn, kpp, address, is_active) VALUES ('ООО "РЕСУРС"', 'Общество с ограниченной ответственностью "Ресурс"', '754839403230', '334433212', 'ул. Мира 7б', false);
+
+INSERT INTO Office (org_id, name, address, phone, is_active) VALUES (1, 'Победа', 'ул. Ленина 7', '60-40-30', true);
+
+INSERT INTO Office (org_id, name, address, phone, is_active) VALUES (1, 'Звезда', 'ул. Лунина 7а', false);
+
+INSERT INTO Office (org_id, name, address) VALUES (2, 'Компас', 'ул. Пушкина 19');
+
+INSERT INTO User (office_id, first_name, last_name, middle_name, position, phone, user_docs_id, citizenship_id, is_identified) VALUES (1, 'Илья', 'Климов', 'Андреевич', 'слесарь', '84394959340', 1, 7, true);
+
+INSERT INTO User (office_id, first_name, last_name, middle_name, position, phone) VALUES (2, 'Андрей', 'Капустин', 'Олегович', 'механик', '83842095858',);
+
+INSERT INTO User (office_id, first_name, middle_name, position, user_docs_id, citizenship_id, is_identified) VALUES (2, 'Владимир', 'Владимирович', 'строитель', 2, 4, true);
+
+INSERT INTO User (office_id, first_name, last_name, middle_name, position, phone, citizenship_id, is_identified) VALUES (3, 'Анастасия', 'Харитонова', 'Олеговна', 'менеджер', '85984574637', 2, false);
+
+INSERT INTO User_Doc (docs_id, doc_date, doc_number) VALUES (7, '04-06-2000', '49385859384');
+
+INSERT INTO User_Doc (docs_id, doc_date, doc_number) VALUES (4, '15-07-2011', '98580048659');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (03, 'Свидетельство о рождении');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (07, 'Военный билет');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (08, 'Временное удостоверение, выданное взамен военного билета');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (10, 'Паспорт иностранного гражданина');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (11, 'Свидетельство о рассмотрении ходатайства о признании лица беженцем на территории Российской Федерации по существу');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (12, 'Вид на жительство в Российской Федерации');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (13, 'Удостоверение беженца');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (15, 'Разрешение на временное проживание в Российской Федерации');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (18, 'Свидетельство о предоставлении временного убежища на территории Российской Федерации');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (21, 'Паспорт гражданина Российской Федерации');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (23, 'Свидетельство о рождении, выданное уполномоченным органом иностранного государства');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (24, 'Удостоверение личности военнослужащего Российской Федерации');
+
+INSERT INTO Docs (doc_code, doc_name) VALUES (91, 'Иные документы');
+
+INSERT INTO Countries (code, name) VALUES (031, 'АЗЕРБАЙДЖАН');
+
+INSERT INTO Countries (code, name) VALUES (076, 'БРАЗИЛИЯ');
+
+INSERT INTO Countries (code, name) VALUES (124, 'КАНАДА');
+
+INSERT INTO Countries (code, name) VALUES (154, 'КИТАЙ');
+
+INSERT INTO Countries (code, name) VALUES (208, 'ДАНИЯ');
+
+INSERT INTO Countries (code, name) VALUES (250, 'ФРАНЦИЯ');
+
+INSERT INTO Countries (code, name) VALUES (643, 'РОССИЯ');
+
+INSERT INTO Countries (code, name) VALUES (682, 'САУДОВСКАЯ АРАВИЯ');
+
+INSERT INTO Countries (code, name) VALUES (752, 'ШВЕЦИЯ');
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,79 @@
+CREATE TABLE IF NOT EXISTS Organization (
+    id         INTEGER NOT NULL      COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version    INTEGER DEFAULT 1     COMMENT 'Служебное поле hibernate',
+    name       VARCHAR(50) NOT NULL  COMMENT 'Название'
+    full_name  VARCHAR(50) NOT NULL  COMMENT 'Полное название',
+    inn        VARCHAR(50) NOT NULL  COMMENT 'ИНН'
+    kpp        VARCHAR(50) NOT NULL  COMMENT 'КПП'
+    address    VARCHAR(50) NOT NULL  COMMENT 'Адрес'
+    phone      VARCHAR(50)           COMMENT 'Номер телефона'
+    is_active  BOOLEAN               COMMENT 'Действителен ли'
+);
+COMMENT ON TABLE Organization IS 'Организация';
+
+CREATE TABLE IF NOT EXISTS Office (
+    id         INTEGER NOT NULL     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version    INTEGER DEFAULT 1    COMMENT 'Служебное поле hibernate',
+    org_id     INTEGER NOT NULL     COMMENT 'Уникальный идентификатор организации'
+    name       VARCHAR(50) NOT NULL COMMENT 'Название'
+    address    VARCHAR(50) NOT NULL COMMENT 'Адрес'
+    phone      VARCHAR(50)          COMMENT 'Номер телефона'
+    is_active  BOOLEAN              COMMENT 'Действителен ли'
+);
+COMMENT ON TABLE Office IS 'Офис';
+
+CREATE TABLE IF NOT EXISTS User (
+    id                INTEGER NOT NULL     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version           INTEGER DEFAULT 1    COMMENT 'Служебное поле hibernate',
+    office_id         INTEGER NOT NULL     COMMENT 'Уникальный идентификатор офиса'
+    first_name        VARCHAR(50) NOT NULL COMMENT 'Имя'
+    last_name         VARCHAR(50)          COMMENT 'Фамилия'
+    middle_name       VARCHAR(50)          COMMENT 'Отчество'
+    position          VARCHAR(50) NOT NULL COMMENT 'Позиция'
+    phone             VARCHAR(50)          COMMENT 'Номер телефона'
+    user_docs_id      INTEGER              COMMENT 'Уникальный идентификатор документа пользователя'
+    citizenship_id    INTEGER              COMMENT 'Уникальный идентификатор гражданства'
+    is_identified     BOOLEAN              COMMENT 'Идентифицированный ли'
+);
+COMMENT ON TABLE User IS 'Пользователь';
+
+CREATE TABLE IF NOT EXISTS User_Doc (
+    id         INTEGER NOT NULL     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version    INTEGER DEFAULT 1    COMMENT 'Служебное поле hibernate',
+    docs_id    INTEGER NOT NULL     COMMENT 'Уникальный идентификатор справочника документов',
+    doc_date   TIMESTAMP NOT NULL   COMMENT 'Дата'
+    doc_number VARCHAR(50) NOT NULL COMMENT 'Номер'
+
+);
+COMMENT ON TABLE User_Doc IS 'Документ пользователя';
+
+CREATE TABLE IF NOT EXISTS Docs (
+    id         INTEGER NOT NULL     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version    INTEGER DEFAULT 1    COMMENT 'Служебное поле hibernate',
+    doc_code   INTEGER NOT NULL     COMMENT 'Код',
+    doc_name   VARCHAR(50) NOT NULL COMMENT 'Название документа'
+);
+COMMENT ON TABLE Docs IS 'Справочник документов';
+
+CREATE TABLE IF NOT EXISTS Countries (
+    id         INTEGER NOT NULL     COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version    INTEGER DEFAULT 1    COMMENT 'Служебное поле hibernate',
+    code       INTEGER NOT NULL     COMMENT 'Код',
+    name       VARCHAR(50) NOT NULL COMMENT 'Название страны'
+);
+COMMENT ON TABLE Countries IS 'Справочник стран';
+
+CREATE INDEX IX_Office_org_id ON Office (org_id);
+ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization(id);
+
+CREATE INDEX IX_User_office_id ON User (office_id);
+ALTER TABLE User ADD FOREIGN KEY (office_id) REFERENCES Office(id);
+
+CREATE INDEX IX_User_user_doc_id ON User (user_doc_id);
+ALTER TABLE User ADD FOREIGN KEY (user_docs_id) REFERENCES User_Doc(id);
+
+CREATE INDEX IX_User_citizenship_id ON User (citizenship_id);
+ALTER TABLE User ADD FOREIGN KEY (citizenship_id) REFERENCES Countries(id);
+
+CREATE INDEX IX_User_Doc_docs_id ON User_Doc (docs_id);
+ALTER TABLE User_Doc ADD FOREIGN KEY (docs_id) REFERENCES Docs(id);
